518f3fdc3a47867acddb369126a8ac01e896abc2
Patrick Cole <hpcmobiledev@gmail.com>
2018-01-07 16:22:12 -0500

    Initial commit with starting index file

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+
+    /*
+     * Title: PDO Introduction Sample
+     * Author: Patrick Cole
+     * Description: This sample is to get me started on connecting to databases
+     * using PDO with real examples provided by the source tutorial.
+     * Source: https://phpro.org/tutorials/Introduction-to-PHP-PDO.html
+    */
+
+    
+?>
