727dd9a1a60a0c5f5606e130916cd449688d8497
Patrick Cole <hpcmobiledev@gmail.com>
2018-01-07 16:52:25 -0500

    Commented out previous steps to allow newly added select query to be executed.

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
     */
 
     # First, let's check what drivers are available in our PHP installation:
+    /*
     echo '<ul>';
     
     foreach(PDO::getAvailableDrivers() as $driver){
@@ -18,11 +19,10 @@
 
     echo '</ul>';
 
-    /* 
-        The results were:
-        mysql
-        odbc
-        sqlite
+        # The results were:
+        # mysql
+        # odbc
+        # sqlite
     */
 
     # Let's now establish a connection:
@@ -37,12 +37,22 @@
         # Provide a success message to front-end:
         echo '<p>Connected to Database</p>';
 
+        /*
         # Insert Data sample:
+        # exec() method is best for when no result set is expected:
         $count = $dbh->exec("INSERT INTO animals(animal_type, animal_name) VALUES ('kiwi', 'troy')");
         
         # Show an updated count upon success of executed statement:
         echo '<p>' . $count . ' row(s) updated.</p>';
-        
+        */
+
+        # Now perform a select query:
+        $sql = "SELECT * FROM animals";
+        foreach ( $dbh->query($sql) as $row){
+            
+            print $row['animal_type'] . ' - ' . $row['animal_name'] . '<br />';
+        }
+
         # Close the database connection:
         $dbh = null;
     } catch ( PDOException $error ){
